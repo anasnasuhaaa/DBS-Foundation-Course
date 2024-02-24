cfa0c535c29771fc549f47a3fd348345d5c2112f
Anas Nasuha <infoanaspedia@gmail.com>
2024-02-24 08:21:30 +0700

    finish oop

---
+++ Basic Javascript/oop/builtinclas.js
@@ -0,0 +1,14 @@
+const date = new Date();
+ 
+const timeInJakarta = date.toLocaleString('id-ID', {
+  timeZone: 'Asia/Jakarta',
+});
+ 
+const timeInTokyo = date.toLocaleString('ja-JP', {
+  timeZone: 'Asia/Tokyo',
+});
+ 
+const timeInMakassar = date.toLocaleString('id-ID', {
+  timeZone: 'Asia/Makassar',
+});
+ 
